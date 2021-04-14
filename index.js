c609cd419a052580b2e2e6e28c0acfb09daf01e2
Logan Liffick <ltliffick@gmail.com>
2021-04-14 19:08:43 -0400

    added gitignore

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
-splt = ({ target = ".splt", reveal = false }) => {
+splt = ({ target = '.splt', reveal = false }) => {
   let saveOriginal = [];
 
   //grab entities
   const entity = document.querySelectorAll(target);
 
   for (let a = 0; a < entity.length; a++) {
-    entity[a].setAttribute("id", "i" + [a + 1]);
+    entity[a].setAttribute('id', 'i' + [a + 1]);
 
     //saves original text to an array for revert functionality
     saveOriginal.push(entity[a].innerHTML);
 
     //split entity text
-    const entityChars = entity[a].innerHTML.split("");
+    const entityChars = entity[a].innerHTML.split('');
     for (let b = 0; b < entityChars.length; b++) {
       //nest child span
-      const span = document.createElement("span");
+      const span = document.createElement('span');
       entity[a].appendChild(span);
-      span.setAttribute("id", "c" + [b + 1]);
+      span.setAttribute('id', 'c' + [b + 1]);
 
       //check if child = char or whitespace
-      if (entityChars[b] == " ") {
-        span.classList.add("whtSpc");
+      if (entityChars[b] == ' ') {
+        span.classList.add('whtSpc');
       } else {
-        span.classList.add("char");
+        span.classList.add('char');
         //add char styles
-        const char = document.querySelectorAll(".char");
+        const char = document.querySelectorAll('.char');
         for (let c = 0; c < char.length; c++) {
-          char[c].style.display = "inline-block";
-          char[c].style.overflow = "hidden";
-          char[c].style.verticalAlign = "top";
+          char[c].style.display = 'inline-block';
+          char[c].style.overflow = 'hidden';
+          char[c].style.verticalAlign = 'top';
         }
       }
 
       //reveal init
       if (reveal == true) {
         //nest grandchild span
-        const spanChild = document.createElement("span");
+        const spanChild = document.createElement('span');
         spanChild.innerHTML = entityChars[b]; //set text to grandchild span
         span.appendChild(spanChild);
-        spanChild.setAttribute("id", "r");
-        spanChild.classList.add("reveal");
+        spanChild.setAttribute('id', 'r');
+        spanChild.classList.add('reveal');
         //add charReveal styles
-        const charReveal = document.querySelectorAll(".reveal");
+        const charReveal = document.querySelectorAll('.reveal');
         for (let d = 0; d < charReveal.length; d++) {
-          charReveal[d].style.display = "inherit";
-          charReveal[d].style.overflow = "inherit";
-          charReveal[d].style.verticalAlign = "inherit";
+          charReveal[d].style.display = 'inherit';
+          charReveal[d].style.overflow = 'inherit';
+          charReveal[d].style.verticalAlign = 'inherit';
         }
       } else {
         span.innerHTML = entityChars[b]; //set text to child span
@@ -58,7 +58,7 @@ splt = ({ target = ".splt", reveal = false }) => {
   //undo text splitting
   splt.revert = () => {
     for (let e = 0; e < entity.length; e++) {
-      entity[e].removeAttribute("id");
+      entity[e].removeAttribute('id');
       entity[e].innerHTML = saveOriginal[e]; //sets text to original value
     }
   };
