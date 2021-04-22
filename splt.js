2b0e5d06b9b39d31c334dd3c634a45c3f20a9ed0
Logan Liffick <ltliffick@gmail.com>
2021-04-22 10:44:32 -0400

    naming conventions

--- splt.js
+++ splt.js
@@ -1,1 +1,65 @@
-function splt({target:e=".splt",reveal:t=!1}){let l=[];const n=document.querySelectorAll(e);for(let e=0;e<n.length;e++){n[e].setAttribute("id","i"+[e+1]),l.push(n[e].innerHTML);const i=n[e].innerHTML.split("");for(let l=0;l<i.length;l++){const r=document.createElement("span");if(n[e].appendChild(r),r.setAttribute("id","c"+[l+1])," "==i[l])r.classList.add("whtSpc");else{r.classList.add("char");const e=document.querySelectorAll(".char");for(let t=0;t<e.length;t++)e[t].style.display="inline-block",e[t].style.overflow="hidden",e[t].style.verticalAlign="top"}if(1==t){const e=document.createElement("span");e.innerHTML=i[l],r.appendChild(e),e.setAttribute("id","r"),e.classList.add("reveal");const t=document.querySelectorAll(".reveal");for(let e=0;e<t.length;e++)t[e].style.display="inherit",t[e].style.overflow="inherit",t[e].style.verticalAlign="inherit"}else r.innerHTML=i[l]}n[e].removeChild(n[e].childNodes[0])}splt.revert=(()=>{for(let e=0;e<n.length;e++)n[e].removeAttribute("id"),n[e].innerHTML=l[e]})}
+function splt({ target = '.splt', reveal = false }) {
+  let saveOriginal = [];
+
+  //grab instances
+  const inst = document.querySelectorAll(target);
+
+  for (let a = 0; a < inst.length; a++) {
+    inst[a].setAttribute('id', 'i' + [a + 1]);
+
+    //saves original text to an array for revert functionality
+    saveOriginal.push(inst[a].innerHTML);
+
+    //split instance text
+    const instChars = inst[a].innerHTML.split('');
+    for (let b = 0; b < instChars.length; b++) {
+      //nest child span
+      const span = document.createElement('span');
+      inst[a].appendChild(span);
+      span.setAttribute('id', 'c' + [b + 1]);
+
+      //check if child = char or whitespace
+      if (instChars[b] == ' ') {
+        span.classList.add('whtSpc');
+      } else {
+        span.classList.add('char');
+        //add char styles
+        const char = document.querySelectorAll('.char');
+        for (let c = 0; c < char.length; c++) {
+          char[c].style.display = 'inline-block';
+          char[c].style.overflow = 'hidden';
+          char[c].style.verticalAlign = 'top';
+        }
+      }
+
+      //reveal init
+      if (reveal == true) {
+        //nest grandchild span
+        const spanChild = document.createElement('span');
+        spanChild.innerHTML = instChars[b]; //set text to grandchild span
+        span.appendChild(spanChild);
+        spanChild.setAttribute('id', 'r');
+        spanChild.classList.add('reveal');
+        //add charReveal styles
+        const charReveal = document.querySelectorAll('.reveal');
+        for (let d = 0; d < charReveal.length; d++) {
+          charReveal[d].style.display = 'inherit';
+          charReveal[d].style.overflow = 'inherit';
+          charReveal[d].style.verticalAlign = 'inherit';
+        }
+      } else {
+        span.innerHTML = instChars[b]; //set text to child span
+      }
+    }
+
+    inst[a].removeChild(inst[a].childNodes[0]); // remove initial text input
+  }
+
+  //undo text splitting
+  splt.revert = () => {
+    for (let e = 0; e < inst.length; e++) {
+      inst[e].removeAttribute('id');
+      inst[e].innerHTML = saveOriginal[e]; //sets text to original value
+    }
+  };
+}
